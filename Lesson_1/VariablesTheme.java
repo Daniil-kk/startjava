1bb15f0925a821e089a959d4b9b0f9f91db4bb9b
Daniil_kk <championyshka@gmail.com>
2023-06-16 18:20:47 +0300

    six commit

--- Lesson_1/VariablesTheme.java
+++ Lesson_1/VariablesTheme.java
@@ -1,14 +1,15 @@
 class VariablesTheme {
 
     public static void main(String[] args) {
-              System.out.println("1 - Вывод характеристик компьютера");
-        byte core = 6; // кол-во ядер
-        short cashMemory = 8; //Кэш память
-        int ssd = 256;//Объем ssd
-        long memoryFreq = 2666l;//Частота памяти, большего числа нет
-        float maxFrecClock = 4.0f;//Максимальная тактовая частота
-        double diagonal = 35.5;//Диагональ экрана
-        char symbol = 'k';//Символ
+        System.out.println("1 - Вывод характеристик компьютера");
+        byte core = 6; 
+        short cashMemory = 8; 
+        int ssd = 256;
+        long memoryFreq = 2666l;
+        float maxFrecClock = 4.0f;
+        double diagonal = 35.5;
+        char symbol = 'k';
+        boolean onPC = true;
         System.out.println(core);
         System.out.println(cashMemory);
         System.out.println(ssd);
@@ -16,52 +17,56 @@ class VariablesTheme {
         System.out.println(maxFrecClock);
         System.out.println(diagonal);
         System.out.println(symbol);
+        System.out.println(onPC);
+        System.out.println("");
 
         System.out.println("2 - Расчет стоимости товара со скидкой");
         int penPrice = 100;
         int bookPrice = 200;
-        int sum = penPrice + bookPrice; //сумма
-        int discountAmount = sum / 100 * 11;//сумма скидки
-        int discountPrice = sum - discountAmount;
+        int sum = penPrice + bookPrice;
+        int discountSum = sum / 100 * 11;
+        int discountPrice = sum - discountSum;
         System.out.println(sum);
-        System.out.println(discountAmount);
+        System.out.println(discountSum);
         System.out.println(discountPrice);
+        System.out.println("");
 
         System.out.println("3 -  Вывод слово JAVA");
-        System.out.println("     J      a  v      v   a         \n" +
+        System.out.println("     J      a  v      v   a\n" +
                 "     J     a a  v    v   a a\n" +
                 " J   J    aaaaa  V  V   aaaaa\n" +
                 "  JJ     a     a   V   a     a  ");
+        System.out.println("");
 
         System.out.println("4 -  Вывод min и max значений целых числовых типов");
-        byte max_byte = 127;
-        short max_short = 32767;
-        int max_int = 2147483647;
-        long max_long = 9223372036854775807l;
-        System.out.println(max_byte);//max_byte
-        System.out.println(++max_byte);// ,++i, сразу увеличивает на единицу
-        System.out.println(--max_byte);//--i;
+        byte maxByte = 127;
+        System.out.println(maxByte);
+        System.out.println(++maxByte);
+        System.out.println(--maxByte);
 
-        System.out.println(max_short);//max_short
-        System.out.println(++max_short);
-        System.out.println(--max_short);
+        short maxShort = 32767;
+        System.out.println(maxShort);
+        System.out.println(++maxShort);
+        System.out.println(--maxShort);
 
-        System.out.println(max_int);//max_int
-        System.out.println(++max_int);
-        System.out.println(--max_int);
+        int maxInt = 2147483647;
+        System.out.println(maxInt);
+        System.out.println(++maxInt);
+        System.out.println(--maxInt);
 
-        System.out.println(max_long);//max_long
-        System.out.println(++max_long);
-        System.out.println(--max_long);
+        long maxLong = 9223372036854775807l;
+        System.out.println(maxLong);
+        System.out.println(++maxLong);
+        System.out.println(--maxLong);
+        System.out.println("");
 
         System.out.println("5 - Перестановка значений переменных");
         int num1 = 2;
         int num2 = 5;
-        int num3;
         System.out.println("С помощью третьей переменной");
-        num3 = num1;
+        int tmp = num1;
         num1 = num2;
-        num2 = num3;
+        num2 = tmp;
         System.out.println("num1 " + "= " + 2 + ", num2 " + "= " + 5 + "\n" +
                 "num1 " + "= " + num1 + ", num2 " + "=" + num2);
         System.out.println("С помощью арифметических операций");
@@ -78,32 +83,21 @@ class VariablesTheme {
         num1 ^= num2;
         System.out.println("num1 " + "= " + 2 + " , num2 " + " = " + 5 + "\n" +
                 "num1 " + "= " + num1 + ", num2 " + "= " + num2);
+        System.out.println("");
 
         System.out.println("6 - Вывод символов и их кодов");
-        char symbol1 = '#';
-        char symbol2 = '&';
-        char symbol3 = '@';
-        char symbol4 = '^';
-        char symbol5 = '_';
-        int symb1 = (int) symbol1;
-        int symb2 = (int) symbol2;
-        int symb3 = (int) symbol3;
-        int symb4 = (int) symbol4;
-        int symb5 = (int) symbol5;
-        System.out.println("symbol1 = " + symbol1 + "\n" +
-                symb1);
+        char lattice = '#';
+        char ampersand = '&';
+        char commercialAt = '@';
+        char subdigitallyExclusiveOr = '^';
+        char underlining = '_';
 
-        System.out.println("symbol2 = " + symbol2 + "\n" +
-                symb2);
-
-        System.out.println("symbol3 = " + symbol2 + "\n" +
-                symb3);
-
-        System.out.println("symbol4 = " + symbol4 + "\n" +
-                symb4);
-
-        System.out.println("symbol5 = " + symbol5 + "\n" +
-                symb5);
+        System.out.println(lattice + "-" + (int)lattice + "\n" +
+        ampersand + "-" + (int)ampersand + "\n" +
+        commercialAt + "-" + (int)commercialAt + "\n" +
+        subdigitallyExclusiveOr + "-" + (int)subdigitallyExclusiveOr + '\n' +
+        underlining + "-" + (int)underlining);
+        System.out.println("");
 
         System.out.println("7 - Вывод в консоль ASCII-арт Дюка");
         String slash = "/";
@@ -111,33 +105,33 @@ class VariablesTheme {
         String leftParenthesis = "(";
         String rightParenthesis = ")";
         String backslash = "\\";
-        String augmentedUnderline = "____";
         System.out.println("     " + slash + " " + backslash + "\n" +
                 "    " + slash + "   " + backslash + "\n" +
                 "   " + slash + underscore + leftParenthesis + "  " + rightParenthesis + backslash + "\n" +
                 "  " + slash + "       " + backslash + "\n" +
-                " " + slash + augmentedUnderline + slash + backslash + underscore + underscore + underscore + backslash);
+                " " + slash + underscore + underscore + underscore + underscore + slash + backslash + underscore + underscore +
+        underscore + backslash);
+        System.out.println("");
 
         System.out.println("8 - Вывод количества сотен, десятков и единиц числа");
-        int n = 123;
-        int x = 123 / 100;
-        int y = 123 / 10;
-        int z = 123 / 1;
+        int num = 123;
+        int hundred = num / 100;
+        int dozen = num / 10;
+        int unit = num / 1;
 
-        System.out.println("Число " + n + " содержит:" + "\n" +
-                x + " сотен" + "\n" +
-                y + " десятков" + "\n" +
-                z + " единиц" + "\n" +
-                "сумма его цифр = " + 6 + "\n" +
-                "произведение = " + 6);
+        System.out.println("Число " + num + " содержит:" + "\n" +
+                hundred + " сотен" + "\n" +
+                dozen + " десятков" + "\n" +
+                unit + " единиц" + "\n" +
+                "сумма его цифр = " + (hundred + dozen + unit) +"\n" +
+                "произведение = " + (hundred * dozen * unit));
+        System.out.println("");
 
         System.out.println("9 - Вывод времени");
         int time = 86399;
         int minutes = time % 60;
         int seconds = time % 60;
-        int hours = 86399 % 24;
+        int hours = time % 24;
         System.out.println(hours + ":" + minutes + ":" + seconds);
       }
    }
-    
- 
